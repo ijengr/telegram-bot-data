410bb3c1925bcd7629753b5339353519478c922e
Dadang Hidayat <94dhidayat@gmail.com>
2025-10-20 14:26:02 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,10 @@
 { pkgs }: {
   deps = [
-    pkgs.python310
-    pkgs.python310Packages.pip
+    pkgs.python3
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.virtualenv
+    # Untuk handle SSL certificates
+    pkgs.cacert
+    pkgs.openssl
   ];
 }
